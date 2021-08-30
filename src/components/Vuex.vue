1a767d17003d80d5e8b3a829f26fd0e1c2d12dc3
mugu <1175233520@qq.com>
2021-08-30 14:38:39 +0800

    state gettes

--- src/components/Vuex.vue
+++ src/components/Vuex.vue
@@ -1,14 +1,21 @@
 <template>
     <div>
         <h2>NO.18 Vuex数据共享</h2>
-        <p>组合式api使用store.js的--{{ count }}</p>
-        <button @click="count++">+</button>
+        <p>组合式api使用state的--{{ myCount }}</p>
+        <button @click="myCount++">+</button>
+        <br><hr>
+        <p>state仓库方法调用--{{count}}</p>
+        <button @click="incCount">+</button><br>
+        <input placeholder="设置值" v-model="setValue" type="text" @keyup.enter="setCount(setValue)">
+        <hr>
+        <p>state getter</p>
+        <p>{{reverseMsg}}</p>
     </div>
 </template>
 
 <script>
-import { ref } from 'vue';
-import { useStore } from 'vuex';
+import { ref } from "vue";
+import { useStore,mapState,mapGetters } from "vuex";
 export default {
     name: "vuex",
     // data() {
@@ -16,16 +23,38 @@ export default {
     //         count: this.$store.state.count,
     //     };
     // },
-    setup(){
+    setup() {
         //setup使用store
         const store = useStore();
 
-        let count = ref(store.state.count)
-        
-        return{
-            count
-        }
-    }
+        let myCount = ref(store.state.count);
+        let setValue = ref();
+
+        return {
+            myCount,setValue
+        };
+    },
+    methods: {
+        incCount() {
+            this.$store.commit("incCount");
+        },
+        setCount(num) {
+            this.$store.commit("setCount", num);
+        },
+        setMsg() {
+            this.$store.commit("setMsg", "你好我是改变后的数据");
+        },
+        setActionMsg() {
+            this.$store.dispatch("setMsg", "改变后的数据-action");
+        },
+    },
+    computed: {
+        ...mapState(["count", "list"]),
+        ...mapGetters(["reverseMsg", "num"]),
+        // ...mapGetters({
+        //    revmsg:"reverseMsg"
+        // })
+    },
 };
 </script>
 
--- src/vuex/store.js
+++ src/vuex/store.js
@@ -4,15 +4,36 @@ const store = createStore({
     state(){ //数据
         return{
             count:1,
-            list:["马总","李总"]
+            list:["马总","李总"],
+            msg:"你好vue"
         }
     },
-    mutations:{ //方法
+    mutations:{ //方法 
         incCount(state){
-            state.count++
+            state.count++           
         },
         setCount(state,num){
             state.count=num
+        },
+        setMsg(state,msg){
+            state.msg=msg
+        }
+    }
+    ,getters:{  //计算属性
+        reverseMsg(state){
+            return state.msg.split("").reverse().join("")
+        },
+        num(state){
+            return state.count+10
+        }
+    },actions:{  //执行mutations里面的方法  异步操作放在actions
+        incCount(context){
+            context.commit("incCount") //执行mutations incCount
+        },
+        setMsg({commit},msg){
+            setTimeout(()=>{
+                commit("setMsg",msg) 
+            },1000)
         }
     }
 })
